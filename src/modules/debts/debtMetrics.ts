f11245f2d9b22e4523751550fb59542a09ac2f67
gcasinzapata-design <gc.asin.zapata@gmail.com>
2026-05-26 22:31:57 -0500

    Add debt metrics and interest leakage calculations

---
+++ src/modules/debts/debtMetrics.ts
@@ -0,0 +1,34 @@
+export interface DebtAccount {
+  name: string
+  balance: number
+  tea: number
+}
+
+export function calculateWeightedInterest(
+  debts: DebtAccount[]
+) {
+  const totalDebt = debts.reduce(
+    (sum, debt) => sum + debt.balance,
+    0
+  )
+
+  const weightedInterest = debts.reduce(
+    (sum, debt) => {
+      return sum + debt.balance * debt.tea
+    },
+    0
+  )
+
+  return weightedInterest / totalDebt
+}
+
+export function calculateMonthlyInterestLeakage(
+  debts: DebtAccount[]
+) {
+  return debts.reduce((sum, debt) => {
+    return (
+      sum +
+      (debt.balance * debt.tea) / 12 / 100
+    )
+  }, 0)
+}
